121ddc585c31381a99b9f694d45f79446f798002
MusiurAlam <musiur.opu@gmail.com>
2022-02-28 02:37:39 +0600

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { deleteCookieFromCookieStore, getCookieFromCookieStore } from "../cookies/CookieStore";
+
+const Navbar = () => {
+
+    const router = useRouter();
+
+    const [isSignedIn, setIsSignedIn] = useState(getCookieFromCookieStore("IS_SIGNED_IN"));
+
+    const logoutNow = () => {
+        deleteCookieFromCookieStore("IS_SIGNED_IN");
+        router.push("/");
+    }
+
+    const handleLogout = () => {
+        isSignedIn ? logoutNow() : router.push("/login");
+    }
+
+    useEffect(() => {
+        setIsSignedIn(getCookieFromCookieStore("IS_SIGNED_IN"));
+    }, [router]);
+
+    return (
+        <nav className="navbarStyle">
+            <div>
+                <p>HOCprivate-route</p>
+            </div>
+            <div className="linksInNav">
+                <Link href="/dashboard" passHref>Dashboard</Link>
+                <button className="linkStyle" onClick={handleLogout}>{isSignedIn ? "Logout" : "Login"}</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
